42fb120e24d43bfd4dbb2b2fe8d8978f7bdd8020
promahbubul <mahbublalam500@gmail.com>
2024-07-05 10:55:52 +0600

    Testimonial Sectin created

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Testimonial Button Icon Changes //
+const btnLeft = document.getElementById("btn-left");
+const btnRight = document.getElementById("btn-right");
+const btnImgLeft = document.getElementById("btn-img-left");
+const btnImgRight = document.getElementById("btn-img-right");
+
+// Stracture
+const setChangeImage = (event, image) => {
+  btnLeft.addEventListener(event, () => {
+    btnImgLeft.setAttribute("src", image);
+  });
+};
+
+const setChangeImageRight = (event, image) => {
+  btnRight.addEventListener(event, () => {
+    btnImgRight.setAttribute("src", image);
+  });
+};
+
+// Left Button
+setChangeImage("mouseenter", "./images/left-green-arrow.png");
+
+setChangeImage("mouseleave", "./images/left-arrow.png");
+
+// Right Button
+setChangeImageRight("mouseenter", "./images/white-right-arrow.png");
+
+setChangeImageRight("mouseleave", "./images/right-arrow.png");
+
+console.log(btnLeft);
+
+// Testemonial Slider Functionalit
+
+$(document).ready(function () {
+  $(".testimonial-container").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: ".btn-left",
+    autoplay: true,
+    autoplaySpeed: 3000,
+    
+
+    nextArrow: ".btn-right",
+  });
+});
